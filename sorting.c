3d8f5840cc52b2d1bfb4d4be021deec43f92a5b4
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-02 16:05:47 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+	int i,j,n,temp,a[i];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;i<n;i++)
+		{
+			if(a[i]>a[j]
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=o;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return o;
+	}
+	
+© 2018 GitHub, Inc.
